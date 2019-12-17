65bd408feb5425fd3fce6c0eebf1598798a6ace3
comcloudway <dev.jakob.meier@gmail.com>
2019-12-17 20:30:52 +0100

    started work on tab layout

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-JSHell.setup({})
-Console.src=document.getElementById('console')
-
-let  run = () => {
-  Console.src.innerHTML=""
-  let parts = document.getElementById('editor').value
-  console.log(parts);
-  let line = 0;
-  for (let i of parts) {
-    line++;
-    Console.log(`${JSHell.out.Prefix(line+':')}   ${JSHell.execute(i)}`);
-  }
-}
-
-
-
-
 //UI FEATURES
-const toolkit_toggle = (e) => {
-e.style.backgroundColor="red"
+
+const sidepanel = {
+    menu : {
+        save : documemt.getElementById("btn_save"),
+        create : documemt.getElementById("btn_new"),
+        open : documemt.getElementById("btn_open"),
+        exec : documemt.getElementById("btn_exec")
+    },
+    panel : {
+        time : documemt.getElementById("widget_time"),
+        chars : documemt.getElementById("widget_chars")
+    }
 }
 
+const toolbox = {
+    tab : {
+        debug : documemt.getElementById("debug_tab"),
+        console : documemt.getElementById("console_tab")
+    },
+    btn : {
+        debug : documemt.getElementById("btn_debug_tab"),
+        console : documemt.getElementById("btn_console_tab")
+    }
+}
+
+const editor = document.getElementById("code");
+
+
+
+//Toolbox Buttons
+let toolbox_current_tab = toolbox.tab.debug;
+const toogleToolboxTab = (btn, tab) => {
+    toolbox_current_tab = tab;
+    //reset btn color
+    for (let e in toolbox.btn) {
+        e.style.background="transparent";
+    }
+    btn.style.background="#54a16f"
+    //reset visible tab
+    for (let e in toolbox.tab) {
+        e.style.zIndex=-1;
+    }
+    tab.style.zIndex=0;
+}
+toogleToolboxTab(toolbox.btn.debug,toolbox.tab.debug);
+
+toolbox.btn.debug.addEventListener("click",(e)=>{
+    e.preventDefault();
+    toogleToolboxTab(toolbox.btn.debug,toolbox.tab.debug);
+})
+toolbox.btn.console.addEventListener("click",(e)=>{
+    e.preventDefault();
+    toogleToolboxTab(toolbox.btn.console,toolbox.tab.console);
+})
